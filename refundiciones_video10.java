9f863bb652a2f2c175d0bbcb674bae6d8aab393d
Eduardo <41192980+blueudp@users.noreply.github.com>
2019-01-25 10:14:39 +0100

    Update refundiciones_video10.java

--- refundiciones_video10.java
+++ refundiciones_video10.java
@@ -1,5 +1,13 @@
 public class clase_mates{
      public static void main(String[] args){ // clase propia
-         // double raiz=Math.sqrt(9)
+          double num1 = 5;
+          double exponente=3;
+          
+          int resultado=Math.pow(num1, exponente)
+          
+          int resultado=(int)Math.round(num1); // tipo de dato a transformar entre parentesis
+          System.out.println(resultado);
+          // double raiz=Math.sqrt(9);
+          
      }
 }
