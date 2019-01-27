b7b232d8fbbb7d52770e05671552bc441a0be6f4
Eduardo <41192980+blueudp@users.noreply.github.com>
2019-01-27 14:54:03 +0100

    Update strings_video10.java

--- strings_video10.java
+++ strings_video10.java
@@ -4,6 +4,6 @@ public class Strings_video10 {
         String nombre = "Eduardo";
         System.out.println(nombre + " tiene " + nombre.length() + " caracteres.");
         letras = nombre.length();
-        System.out.println("La ultima letra es " + nombre.charAt(letras - 1)); // letras - 1 ya que charAt empieza a contar en 0  
+        System.out.println("La ultima letra es " + nombre.charAt(letras - 1) + ", la primera es " + nombre.charAt(0)); // letras - 1 ya que charAt empieza a contar en 0  
     }
 }
