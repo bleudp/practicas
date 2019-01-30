4dc8262bd1b393c31a9aaf3000848f0fd79e44a5
Eduardo <41192980+blueudp@users.noreply.github.com>
2019-01-30 12:24:11 +0100

    Create Video28_uso_coche.java

---
+++ Video28_uso_coche.java
@@ -0,0 +1,12 @@
+package poo;
+
+public class uso_coche(){
+
+    public static void main(String[] args){
+    
+    Coche Renault= new Coche(); // instancia de clase
+    
+    System.out.println("el coche tiene " + Renault.ruedas + " ruedas.");
+    }
+
+}
